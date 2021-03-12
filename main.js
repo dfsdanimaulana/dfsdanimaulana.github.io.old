dacd469b5f2b4b79befe4a35566b58a70ed9c152
dfsdanimaulana <danimaulana9f@gmail.com>
2021-03-13 00:59:04 +0700

    ubah dikit

--- main.js
+++ main.js
@@ -2,19 +2,18 @@ swal('On Progress... \:\)','','success');
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbznax9zNXPlvwHyGP6DldW_c8-FLH78ocXcUKePyaD0ermCz-nCs6xYYl-YkvC9If2yKA/exec'
 const form = document.forms['submit-to-google-sheet']
-const btnSend = $('.btn-send');
-const btnLoading = $('.btn-loading');
+const btn = [$('.btn-send'),$('.btn-loading')];
 const myAlert = $('.my-alert');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  btnLoading.toggleClass('d-none');
-  btnSend.toggleClass('d-none');
+  btn[0].toggleClass('d-none');
+  btn[1].toggleClass('d-none');
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
       console.log('Success!', response)
-      btnLoading.toggleClass('d-none');
-      btnSend.toggleClass('d-none');
+      btn[0].toggleClass('d-none');
+      btn[1].toggleClass('d-none');
       myAlert.toggleClass('d-none');
       form.reset();
     })
